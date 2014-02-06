7569ebc5aff2d4af1ad3c6ac0c41c6b37a78cb76
Jason <cse491webdev@T.(none)>
2014-02-06 11:41:36 -0500

    Got some code to finish this hw.  hw4 done.

--- server.py
+++ server.py
@@ -1,146 +1,84 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+import cgi
+import jinja2
 import random
 import socket
-import time
-from urlparse import urlparse
-from urlparse import parse_qs
-import cgi
 from StringIO import StringIO
-import jinja2
+import time
+import urllib
+from urlparse import urlparse, parse_qs
 
+# Taken from leflerja to get this working
 
-#Globals
-app_content = 'Content-type: application/x-www-form-urlencoded\r\n\r\n'
-error_header = 'HTTP/1.0 404 Not Found\r\n'
-multi_content = 'Content-type: multipart/form-data\r\n\r\n'
-okay_header = 'HTTP/1.0 200 OK\r\n'
-text_content = 'Content-type: text/html\r\n\r\n'
-loader = jinja2.FileSystemLoader('./templates')
-env = jinja2.Environment(loader=loader)
+def handle_connection(conn):
+    okay_response = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
+    error_response = 'HTTP/1.0 404 Not Found\r\nContent-type: text/html\r\n\r\n'
+    loader = jinja2.FileSystemLoader('./templates')
+    env = jinja2.Environment(loader=loader)
+    
+    request = conn.recv(1)
+    count = 0
+    while request[-4:] != '\r\n\r\n':
+        request += conn.recv(1)
+    
+    request, headers = request.split('\r\n',1)
+    d = {}
+    for line in headers.split('\r\n')[:-2]:
+        k, v = line.split(': ', 1)
+        d[k.lower()] = v
+
+    request_type = request.split()[0]
+    path = urlparse(request.split(' ', 3)[1])
+    page = path[2]
+
+    my_pages = {'/' : 'index.html', \
+                '/content' : 'content.html', \
+                '/file' : 'files.html', \
+                '/image' : 'image.html', \
+                '/form' : 'get_form.html', \
+                '/submit' : 'get_submit.html' }
+
+    # Check for POST
+    body = ''
+    if request_type == 'POST':
+        while len(body) < int(d['content-length']):
+            body += conn.recv(1)
+        e = {'REQUEST_METHOD' : 'POST'}
+        fs = cgi.FieldStorage(fp=StringIO(body), headers=d, environ=e)
+        params = {}
+        for key in fs.keys():
+            params[key] = fs[key].value
+    else:
+        params = parse_qs(path[4])
+        
+    # Create and send response
+    if page not in my_pages:
+        template = env.get_template('error.html')
+        error_response += template.render()
+        conn.send(error_response)
+    else:
+        template = env.get_template(my_pages[page])
+        okay_response += template.render(params)
+        conn.send(okay_response)
+    conn.close()
 
 def main():
-    s = socket.socket()         # Create a socket object
+    s = socket.socket() # Create a socket object
     host = socket.getfqdn() # Get local machine name
     port = random.randint(8000, 9999)
-    s.bind((host, port))        # Bind to the port
+    s.bind((host, port)) # Bind to the port
 
     print 'Starting server on', host, port
     print 'The Web server URL for this would be http://%s:%d/' % (host, port)
-
-    s.listen(5)                 # Now wait for client connection.
+    
+    s.listen(5) # Now wait for client connection
 
     print 'Entering infinite loop; hit CTRL-C to exit'
-    
     while True:
-        # Establish connection with client.    
+        # Establish connection with client
         c, (client_host, client_port) = s.accept()
+        print 'Got connection from', client_host, client_port
         handle_connection(c)
-        
-
-# Handles the connection
-def handle_connection(conn):
-    info = conn.recv(1)
-    while info[-4:] != '\r\n\r\n':
-        info += conn.recv(1)
-    
-    request = info.split(' ')
-    urlRequest = request[1]
-    urlInfo = urlparse(urlRequest)
-    urlPath = urlInfo.path
-
-    if request[0] == 'GET':
-        handle_get(conn, urlPath, info)
-    elif request[0] == 'POST':
-        handle_post(conn, urlPath)
-    conn.close()
-
-# Taken from leflerja
-def handle_get(conn, path, info):
-    params = parse_qs(urlparse(path)[4])
-    page = urlparse(path)[2]
-    vars_dict = {}
-    get_pages = { '/'  :  'index.html',  \
-                  '/favicon.ico'  :  'index.html',  \
-                  '/content'  : 'content.html',  \
-                  '/file'  :  'files.html',  \
-                  '/image'  :  'image.html',  \
-                  '/form'  :  'get_form.html',  \
-                  '/submit'  :  'get_submit.html'  }
-    print page
-    if page in get_pages:
-        template = env.get_template(get_pages[page])
-        if(page == "/submit"):
-            firstName, lastName = handle_submit(conn, urlparse(info.split(' ')[1]), info, info.split(' ')[0])
-            vars_dict = {'firstName': firstName, 'lastName': lastName}
-        conn.send(okay_header)
-        conn.send(text_content)
-        conn.send(template.render(vars_dict))
-    else:
-        template = env.get_template('error.html')
-        conn.send(error_header)
-        conn.send(text_content)
-        conn.send(template.render())
-    
-# Taken from leflerja
-def handle_post(conn, path, info):
-    headers = []
-    body = ""
-    has_body = False
-
-    for line in request.split("\r\n"):
-        if has_body:
-            body = line
-            continue
-        if line == "":
-            has_body = True
-        headers.append(line)
-
-    path = request.split()[1]
-    page = urlparse(path)[2]
-    params = parse_qs(body)
-
-    post_pages = {'/' : post_request, \
-                  '/form' : post_form, \
-                  '/submit' : post_submit }
-
-    conn.send(okay_header)
-    print params
-    conn.send(app_content)
-    conn.send(post_pages[page](conn, params))
-
-def handle_submit(conn, urlInfo, info, reqType):
-    if reqType == "GET":
-        query = urlInfo.query
-    elif reqType == "POST":
-        query = info.splitlines()[-1]
-        
-    data = parse_qs(query)
-    firstName = data['firstName'][0]
-    lastName = data['lastName'][0]
-    return (firstName, lastName)
-
-def handle_post(conn, info):
-    toSend = 'HTTP/1.0 200 OK\r\n' + \
-             'Content-type: text/html\r\n\r\n' + \
-             '<h2>hello world</h2>'
-    conn.send(toSend)
-
-def post_request(conn, request):
-    return 'HTTP/1.0 200 OK\r\n\r\n' + \
-           'This is a post request'
-
-def post_form(conn, params):
-    return '<h1>Form Page</h1>\r\n' + \
-           '<form action=\'/submit\' method=\'POST\'>\r\n' + \
-           'First Name: <input type=\'text\' name=\'firstname\'><br>\r\n' + \
-           'Last Name: <input type=\'text\' name=\'lastname\'><br>\r\n' + \
-           '<input type=\'submit\' name=\'submit\'>\r\n' + \
-           '</form>\r\n'
-
-def post_submit(conn, params):
-    return '<h1>Submit Page</h1>\r\n' + \
-           'Hello {0} {1}'.format(params['firstname'][0], params['lastname'][0])
-    
 
 if __name__ == '__main__':
     main()
--- test_server.py
+++ test_server.py
@@ -1,10 +1,109 @@
+#! /usr/bin/env python
 import server
+
+def test_error():
+    conn = FakeConnection("GET /error HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if 'HTTP/1.0 404 Not Found' not in result:
+        assert False
+    else:
+        pass
+
+def test_index():
+    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if ('HTTP/1.0 200 OK' and \
+        'Content-type: text/html' and \
+        'Hello, world.') not in result:
+        assert False
+    else:
+        pass
+
+def test_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if ('HTTP/1.0 200 OK' and \
+        'Content-type: text/html' and \
+        'content page') not in result:
+        assert False
+    else:
+        pass
+
+def test_files():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if ('HTTP/1.0 200 OK' and \
+        'Content-type: text/html' and \
+        'file page') not in result:
+        assert False
+    else:
+        pass
+
+def test_images():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if ('HTTP/1.0 200 OK' and \
+        'Content-type: text/html' and \
+        'image page') not in result:
+        assert False
+    else:
+        pass
+
+def test_form():
+    conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if ('HTTP/1.0 200 OK' and \
+        'Content-type: text/html' and \
+        '<form action=\'/submit\' method=\'GET\'>\r\n' and \
+        'First Name: <input type=\'text\' name=\'firstName\'><br>\r\n' and \
+        'Last Name: <input type=\'text\' name=\'lastName\'><br>\r\n' and \
+        '<input type=\'submit\' name=\'submit\'>\r\n' and \
+        '</form>') not in result:
+        assert False
+    else:
+        pass
+
+def test_submit():
+    conn = FakeConnection("GET /submit?firstName=Jason&lastName=Bull&submit=Submit HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if ('HTTP/1.0 200 OK' and \
+        'Hello Mr. Jason Bull') not in result:
+        assert False
+    else:
+        pass
+
+def test_post_app():
+    conn = FakeConnection("POST /submit HTTP/1.0\r\n" + \
+                          "Content-Length: 31\r\n" + \
+                          "Content-Type: application/x-www-form-urlencoded\r\n\r\n" + \
+                          "firstName=Jason&lastName=Bull\r\n")
+    server.handle_connection(conn)
+    result = conn.sent
+
+    if 'HTTP/1.0 200 OK' not in result:
+        assert False
+    else:
+        pass
 
 class FakeConnection(object):
     """
-    A fake connection class that mimics a real TCP socket for the purpose
-    of testing socket I/O.
-    """
+A fake connection class that mimics a real TCP socket for the purpose
+of testing socket I/O.
+"""
     def __init__(self, to_recv):
         self.to_recv = to_recv
         self.sent = ""
@@ -22,113 +121,8 @@ class FakeConnection(object):
     def send(self, s):
         self.sent += s
 
+    def settimeout(self, n):
+        self.timeout = n
+
     def close(self):
         self.is_closed = True
-
-# Test a basic GET call.
-
-def test_handle_index():
-    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html' and \
-       'Hello, world.') not in result:
-        assert False
-    else:
-        pass
-
-def test_handle_content():
-    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
-    
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html' and \
-       'This is the content page!') not in result:
-        assert False
-    else:
-        pass
-
-def test_handle_file():
-    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html' and \
-       'This is the file page!') not in result:
-        assert False
-    else:
-        pass
-    
-def test_handle_image():
-    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html' and \
-       'This is the image page!') not in result:
-        assert False
-    else:
-        pass
-    
-def test_handle_form():
-    conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html') not in result:
-        assert False
-    else:
-        pass
-    
-
-def test_post_request():
-    conn = FakeConnection("POST /image HTTP/1.0\r\n\r\n")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html') not in result:
-        assert False
-    else:
-        pass
-
-def test_handle_get_submit():
-    conn = FakeConnection("GET /submit?firstName=Jason&lastName=Bull HTTP/1.0\r\n\r\n")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html' and \
-       'Hello Mr. Jason Bull') not in result:
-        assert False
-    else:
-        pass
-
-def test_handle_post_submit():
-    conn = FakeConnection("POST /submit HTTP/1.0\r\n\r\nfirstName=Jason&lastName=Bull")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 200 OK' and \
-       'Content-type: text/html') not in result:
-        assert False
-    else:
-        pass
-
-def test_handle_no_page():
-    conn = FakeConnection("GET /youdontexist HTTP/1.0\r\n\r\n")
-
-    server.handle_connection(conn)
-    result = conn.sent
-    if('HTTP/1.0 404 Not Found' and \
-       'Content-type: text/html' and \
-       'This page does not exist!') not in result:
-        assert False
-    else:
-        pass
