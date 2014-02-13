ddfe2de32f5be8055629af06fea9c97d2b549cbe
Jason <cse491webdev@T.(none)>
2014-02-13 10:42:24 -0500

    hw5

--- app.py
+++ app.py
@@ -1,22 +1,56 @@
-# from http://docs.python.org/2/library/wsgiref.html
+# encoding: utf-8
+# taken from brtaylor92
 
-from wsgiref.util import setup_testing_defaults
+import jinja2
+from urlparse import parse_qs
+from urllib import unquote
+import cgi
 
-# A relatively simple WSGI application. It's going to print out the
-# environment dictionary after being updated by setup_testing_defaults
-def simple_app(environ, start_response):
-    setup_testing_defaults(environ)
+def app(environ, start_response):
+    # The dict of pages we know how to get to
+    response = {
+                '/' : 'index.html', \
+                '/content' : 'content.html', \
+                '/file' : 'files.html', \
+                '/image' : 'images.html', \
+                '/form' : 'form.html', \
+                '/submit' : 'submit.html', \
+               }
 
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
+    # Basic connection information and set up templates
+    loader = jinja2.FileSystemLoader('./templates')
+    env = jinja2.Environment(loader=loader)
+    response_headers = [('Content-type', 'text/html')]
 
-    start_response(status, headers)
+    # Check if we got a path to an existing page
+    if environ['PATH_INFO'] in response:
+        status = '200 OK'
+        template = env.get_template(response[environ['PATH_INFO']])
+    else:
+        status = '404 Not Found'
+        template = env.get_template('error.html')
 
-    ret = ["%s: %s\n" % (key, value)
-           for key, value in environ.iteritems()]
-    ret.insert(0, "This is your environ.  Hello, world!\n\n")
+    # Set up template arguments
+    x = parse_qs(environ['QUERY_STRING']).iteritems()
+    args = {k : v[0] for k,v in x}
+    args['path'] = environ['PATH_INFO']
 
-    return ret
+    # Grab POST args if there are any
+    if environ['REQUEST_METHOD'] == 'POST':
+        headers = {k[5:].lower().replace('_','-') : v \
+                    for k,v in environ.iteritems() if(k.startswith('HTTP'))}
+        headers['content-type'] = environ['CONTENT_TYPE']
+        headers['content-length'] = environ['CONTENT_LENGTH']
+        fs = cgi.FieldStorage(fp=environ['wsgi.input'], \
+                                headers=headers, environ=environ)
+        args.update({x : fs[x].value for x in fs.keys()})
+
+    args = {k : v.decode('cp1252').encode('ascii', 'xmlcharrefreplace') \
+            for k,v in args.iteritems()}
+    
+    # Return the page
+    start_response(status, response_headers)
+    return [bytes(template.render(args))]
 
 def make_app():
-    return simple_app
+    return app
--- test_server.py
+++ test_server.py
@@ -1,103 +1,4 @@
-#! /usr/bin/env python
 import server
-
-def test_error():
-    conn = FakeConnection("GET /error HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if 'HTTP/1.0 404 Not Found' not in result:
-        assert False
-    else:
-        pass
-
-def test_index():
-    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if ('HTTP/1.0 200 OK' and \
-        'Content-type: text/html' and \
-        'Hello, world.') not in result:
-        assert False
-    else:
-        pass
-
-def test_content():
-    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if ('HTTP/1.0 200 OK' and \
-        'Content-type: text/html' and \
-        'content page') not in result:
-        assert False
-    else:
-        pass
-
-def test_files():
-    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if ('HTTP/1.0 200 OK' and \
-        'Content-type: text/html' and \
-        'file page') not in result:
-        assert False
-    else:
-        pass
-
-def test_images():
-    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if ('HTTP/1.0 200 OK' and \
-        'Content-type: text/html' and \
-        'image page') not in result:
-        assert False
-    else:
-        pass
-
-def test_form():
-    conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if ('HTTP/1.0 200 OK' and \
-        'Content-type: text/html' and \
-        '<form action=\'/submit\' method=\'GET\'>\r\n' and \
-        'First Name: <input type=\'text\' name=\'firstName\'><br>\r\n' and \
-        'Last Name: <input type=\'text\' name=\'lastName\'><br>\r\n' and \
-        '<input type=\'submit\' name=\'submit\'>\r\n' and \
-        '</form>') not in result:
-        assert False
-    else:
-        pass
-
-def test_submit():
-    conn = FakeConnection("GET /submit?firstName=Jason&lastName=Bull&submit=Submit HTTP/1.0\r\n\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if ('HTTP/1.0 200 OK' and \
-        'Hello Mr. Jason Bull') not in result:
-        assert False
-    else:
-        pass
-
-def test_post_app():
-    conn = FakeConnection("POST /submit HTTP/1.0\r\n" + \
-                          "Content-Length: 31\r\n" + \
-                          "Content-Type: application/x-www-form-urlencoded\r\n\r\n" + \
-                          "firstName=Jason&lastName=Bull\r\n")
-    server.handle_connection(conn)
-    result = conn.sent
-
-    if 'HTTP/1.0 200 OK' not in result:
-        assert False
-    else:
-        pass
 
 class FakeConnection(object):
     """
@@ -121,8 +22,115 @@ of testing socket I/O.
     def send(self, s):
         self.sent += s
 
-    def settimeout(self, n):
-        self.timeout = n
-
     def close(self):
         self.is_closed = True
+
+def test_handle_connection_slash():
+    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_file():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_handle_connection_image():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_get_form():
+    conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_404():
+    conn = FakeConnection("GET /404 HTTP/1.0\r\n\r\n")
+    er = 'HTTP/1.0 404 Not Found\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_submit_get():
+    fname = "Jason"
+    lname = "Bull"
+    conn = FakeConnection("GET /submit?firstname={0}&lastname={1} \
+HTTP/1.0\r\n\r\n".format(fname, lname))
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_submit_post_urlencoded():
+    fname = "Jason"
+    lname = "Bull"
+    conn = FakeConnection("POST /submit HTTP/1.0\r\n" + \
+                           "Content-Length: 29\r\n" + \
+                           "Content-Type: application/x-www-form-urlencoded\r\n\r\n" + \
+                           "firstname={0}&lastname={1}\r\n".format(fname, lname))
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_submit_post_multipart():
+    conn = FakeConnection("POST /submit HTTP/1.0\r\n" + \
+                          "Content-Length: 374\r\n" + \
+                          "Content-Type: multipart/form-data; " + \
+                          "boundary=32452685f36942178a5f36fd94e34b63\r\n\r\n" + \
+                          "--32452685f36942178a5f36fd94e34b63\r\n" + \
+                          "Content-Disposition: form-data; name=\"lastname\";" + \
+                          " filename=\"lastname\"\r\n\r\n" + \
+                          "Bull\r\n" + \
+                          "--32452685f36942178a5f36fd94e34b63\r\n" + \
+                          "Content-Disposition: form-data; name=\"firstname\";" + \
+                          " filename=\"firstname\"\r\n\r\n" + \
+                          "Jason\r\n" + \
+                          "--32452685f36942178a5f36fd94e34b63\r\n" + \
+                          "Content-Disposition: form-data; name=\"key\";" + \
+                          " filename=\"key\"\r\n\r\n" + \
+                          "value\r\n" + \
+                          "--32452685f36942178a5f36fd94e34b63--\r\n"
+                    )
+    fname = 'Jason'
+    lname = 'Bull'
+    er = 'HTTP/1.0 200 OK\r\n'
+
+    server.handle_connection(conn)
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
+
+def test_submit_post_404():
+    conn = FakeConnection("POST /asdf HTTP/1.0\r\n" + \
+                          "Content-Length: 0\r\n" + \
+                          "Content-Type: application/x-www-form-urlencoded\r\n\r\n"
+                         )
+    server.handle_connection(conn)
+
+    er = 'HTTP/1.0 404 Not Found\r\n'
+
+    assert conn.sent[:len(er)] == er, 'Got: %s' % (repr(conn.sent),)
